4bd6eddcc080fd914c9e3055fb4c85608e1716db
Roberto <roberto.britz@hotmail.com>
2024-05-25 16:54:26 -0300

    Configurando Seeder de acesso para o ADM

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@nex.eco.br',
+            'password' => bcrypt('123456789'),
         ]);
+        Role::create([
+            'name' => 'Admin',
+        ]);
+        Role::create([
+            'name' => 'Gerente',
+        ]);
+        Role::create([
+            'name' => 'Usuario',
+        ]);
+        Permission::create([
+            'name' => 'permission_edit',
+        ]);
+        Permission::create([
+            'name' => 'user_edit',
+        ]);
+        Permission::create([
+            'name' => 'role_edit',
+        ]);
+
+        DB::table('role_has_permissions')->insert([
+            [
+                'permission_id' => '1',
+                'role_id' => '1',
+            ]
+        ]);
+        DB::table('role_has_permissions')->insert([
+            [
+                'permission_id' => '2',
+                'role_id' => '1',
+            ]
+        ]);
+        DB::table('role_has_permissions')->insert([
+            [
+                'permission_id' => '3',
+                'role_id' => '1',
+            ]
+        ]);
+        DB::table('model_has_roles')->insert([
+            [
+                'role_id' => '1',
+                'model_type' => 'App\Models\User',
+                'model_id' => '1'
+            ]
+        ]);
+
     }
 }
